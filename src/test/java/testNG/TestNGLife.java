996b9e5463f405fa266ee12a84803a945041be04
Summer-PC <1270102724@qq.com>
2020-11-11 18:28:42 +0800

    testng 注解生命周期

--- src/test/java/testNG/TestNGLife.java
+++ src/test/java/testNG/TestNGLife.java
@@ -1,5 +1,7 @@
 package testNG;
 
+import org.testng.annotations.*;
+
 /**
  * @author mumu
  * @Title: ${file_name}
@@ -8,4 +10,44 @@ package testNG;
  * @date 2020/11/1117:42
  */
 public class TestNGLife {
+    @BeforeSuite
+    public void beforSuite(){
+        System.out.println("beforSuite");
+    }
+    @AfterSuite
+    public void afterSuite(){
+        System.out.println("afterSuite");
+    }
+    @BeforeTest
+    public void beforTest(){
+        System.out.println("beforTest");
+    }
+    @AfterTest
+    public void afterTest(){
+        System.out.println("afterTest");
+    }
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("beforeClass");
+    }
+    @AfterClass
+    public void afterClass(){
+        System.out.println("afterClass");
+    }
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("beforeMethod");
+    }
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("afterMethod");
+    }
+    @Test(enabled = false)
+    public void test01(){
+        System.out.println("test01");
+    }
+    @Test
+    public void test02(){
+        System.out.println("test02");
+    }
 }
