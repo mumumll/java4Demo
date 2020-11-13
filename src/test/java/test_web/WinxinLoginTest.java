ccfa9c0a700927d5de28ea9418f82d6f768a8c8f
mumumll <1270102724@qq.com>
2020-11-13 23:21:34 +0800

    实现cookies登录企业微信

--- src/test/java/test_web/WinxinLoginTest.java
+++ src/test/java/test_web/WinxinLoginTest.java
@@ -2,10 +2,17 @@ package test_web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.core.type.TypeReference;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -16,8 +23,11 @@ import java.util.concurrent.TimeUnit;
  */
 public class WinxinLoginTest extends BaseTest{
 
+
+
     @Test
-    void login() throws IOException {
+    void loginTest() throws IOException {
+
         // 隐士等待5秒
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         // 打开企业微信扫码页面
@@ -27,6 +37,24 @@ public class WinxinLoginTest extends BaseTest{
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
         mapper.writeValue(new File("cookies.yaml"), cookies);
 
+    }
+    @Test
+    void loginedTest() throws IOException {
+
+        // 隐士等待5秒
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        // 打开企业微信扫码页面
+        driver.get("https://work.weixin.qq.com/wework_admin/frame");
+        sleep(10000);
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        TypeReference typeReference=new TypeReference<List<HashMap<String, Object>>>() {};
+        List<HashMap<String, Object>> cookies = (List<HashMap<String, Object>>) mapper.readValue(new File("cookies.yaml"),typeReference);
+        System.out.println(cookies);
+        cookies.forEach(cookieMap->{
+            driver.manage().addCookie(new Cookie(cookieMap.get("name").toString(), cookieMap.get("value").toString()));
+        });
+
+        driver.navigate().refresh();
     }
 
     /**
