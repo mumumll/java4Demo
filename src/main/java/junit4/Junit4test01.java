24f35dae2247f18783434229c9e66b2de3445ed5
Summer-PC <1270102724@qq.com>
2020-11-17 11:17:49 +0800

    移除testng和junit4的代码

--- src/main/java/junit4/Junit4test01.java
+++
@@ -1,9 +0,0 @@
-package junit4;
-
-
-public class Junit4test01 {
-
-    public void fun1(){
-        System.out.println("fun0101010101");
-    }
-}
--- src/test/java/junit4/testCase/BaseTest.java
+++
@@ -1,7 +0,0 @@
-package junit4.testCase;
-
-import java.util.HashMap;
-
-public class BaseTest {
-    protected static HashMap<String,Object> dataMap = new HashMap<String, Object>();
-}
